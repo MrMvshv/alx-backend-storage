cb4aeb1331262c152f1b7880e8f8cfe0a57d74ee
MrMvshv <machariajosphat001@gmail.com>
2023-07-14 20:15:30 +0000

    sql create table, unique

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -2,7 +2,7 @@
 -- If the table already exists, the script should not fail
 
 CREATE TABLE IF NOT EXISTS users (
-  id INTEGER PRIMARY KEY NOT NULL AUTOINCREMENT,
+  id INTEGER PRIMARY KEY NOT NULL AUTO_INCREMENT,
   email VARCHAR(255) NOT NULL UNIQUE,
   name VARCHAR(255)
 );
